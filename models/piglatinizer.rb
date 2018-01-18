bf94233633b7551b8e30e2386da280c84abff8fb
Meredith Reece <mardebugger@gmail.com>
2018-01-18 23:47:13 +0000

    automatically backed up by learn

---
+++ models/piglatinizer.rb
@@ -0,0 +1,13 @@
+class PigLatinizer
+
+  def pig_latinize_this(word)
+
+  end
+
+  def to_pig_latin(phrase)
+    # split into array
+    # run a method on each word
+    # join back into a string
+  end
+
+end
