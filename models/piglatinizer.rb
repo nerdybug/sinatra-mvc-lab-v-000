4b8aefa27f1196b2cea0b9f0cb78cc71b981f7b9
Meredith Reece <mardebugger@gmail.com>
2018-01-19 00:15:08 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,7 +1,15 @@
 class PigLatinizer
 
   def pig_latinize_this(word)
+    if condition #the word starts with a vowel
 
+    else #the word starts with a consonant
+      split = word.split(/([aeiouAEIOU].*)/)
+      # split[0] => the consonants at the beginning of the word
+      # split[1] => the rest of the word
+      re_order = [split[1], split[0], "ay"]
+      re_order.join("")
+    end
   end
 
   def to_pig_latin(phrase)
