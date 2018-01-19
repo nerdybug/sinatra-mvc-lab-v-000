f4bb6af67939172b2723d12de000077ba08b6e27
Meredith Reece <mardebugger@gmail.com>
2018-01-19 00:09:53 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,12 +1,6 @@
 class PigLatinizer
 
   def pig_latinize_this(word)
-    if !!(word =~ /(\b[^AaEeIiOoUu\s][^AaEeIiOoUu\s])(\w+)/) #two_consonants
-      word.gsub!(/(\b[^AaEeIiOoUu\s][^AaEeIiOoUu\s])(\w+)/, /\2\1ay/)
-    elsif !!(word =~ /(\b[^AaEeIiOoUu\s])([AaEeIiOoUu\s]\w+)/) #one_consonant
-
-    elsif !!(word =~ /(\b[aeiou]\w+)/) #vowel
-
 
   end
 
