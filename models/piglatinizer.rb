6b5d16df2a3ccbb3d9894847fe06628870b6e14b
Meredith Reece <mardebugger@gmail.com>
2018-01-19 00:07:17 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,9 +1,9 @@
 class PigLatinizer
 
   def pig_latinize_this(word)
-    if !!(word =~ /(\b[^aeiou\s][^aeiou\s])(\w+)/) #two_consonants
-      word.gsub!(/(\b[^aeiou\s][^aeiou\s])(\w+)/, /\2\1ay/)
-    elsif !!(word =~ /(\b[^aeiou\s])([aeiou\s]\w+)/) #one_consonant
+    if !!(word =~ /(\b[^AaEeIiOoUu\s][^AaEeIiOoUu\s])(\w+)/) #two_consonants
+      word.gsub!(/(\b[^AaEeIiOoUu\s][^AaEeIiOoUu\s])(\w+)/, /\2\1ay/)
+    elsif !!(word =~ /(\b[^AaEeIiOoUu\s])([AaEeIiOoUu\s]\w+)/) #one_consonant
 
     elsif !!(word =~ /(\b[aeiou]\w+)/) #vowel
 
@@ -11,7 +11,7 @@ class PigLatinizer
   end
 
   def to_pig_latin(phrase)
-    words = phrase.downcase.split(" ")
+    words = phrase.split(" ")
     # run a method on each word
     # join back into a string
   end
